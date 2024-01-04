5cc4dc683f9569ca61146201f4d7ef041e4145ee
Danilo <magliadanilo@gmail.com>
2024-01-04 12:19:02 +0100

    fixed path in php file and profilepage.html

--- php/requests/user/load_comments.php
+++ php/requests/user/load_comments.php
@@ -1,8 +1,8 @@
 <?php
     use Post\PostUtility;
-    require_once(getenv("PL_ROOTDIRECTORY")."../php/bootstrap.php");
-    require_once(getenv("PL_ROOTDIRECTORY")."../db/post.php");
-    require_once(getenv("PL_ROOTDIRECTORY")."../php/requests/cors.php");
+    require_once(getenv("PL_ROOTDIRECTORY")."php/bootstrap.php");
+    require_once(getenv("PL_ROOTDIRECTORY")."db/post.php");
+    require_once(getenv("PL_ROOTDIRECTORY")."php/requests/cors.php");
     global $driver;
     $post = $_GET['post'];
     $comments = PostUtility::comments_with_post($driver, $post);
