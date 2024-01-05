1505f186cd73e7b0201a05541f878867f478b246
Danilo <magliadanilo@gmail.com>
2024-01-05 15:58:19 +0100

    Finished the first implementation of the login page, changed the behavior of the authentication to set a cookie with the token

--- static/js/login.js
+++ static/js/login.js
@@ -1,12 +1,21 @@
-function tryLogin() {
-    username = document.getElementById("username")
-    password = document.getElementById("password")
+async function tryLogin() {
+    username = document.getElementById("username").value
+    password = document.getElementById("password").value
     //fetch("https://api.partylinker.live/auth/auth.php", {
-    fetch("http://localhost/php/requests/auth/auth.php", {
+    const response = await fetch("http://localhost:8080/php/requests/auth/auth.php", {
         method: "POST",
-        body: {
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
             "username": username,
             "password": password
-        }
+        })
     })
+    const data = await response.json()
+    if(!data.hasOwnProperty("error")) {
+        window.location.href = "/"
+    } else {
+        //TODO: Show error message
+    }
 }
