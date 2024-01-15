862ac9b0820e978c10c928208535fa2dca27a457
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2024-01-15 13:08:58 +0100

    small fix about profilepage and modals

--- static/js/profilepage.js
+++ static/js/profilepage.js
@@ -1,6 +1,35 @@
 import { request_path } from "/static/js/config.js";
 import { loadUserImage } from "/static/js/utils.js";
 
+const postButton = document.getElementById("buttons").getElementsByTagName("div").item(0)
+const eventButton = document.getElementById("buttons").getElementsByTagName("div").item(1);
+postButton.addEventListener("click", changePosted);
+eventButton.addEventListener("click", changeEvents);
+
+let posted = true;
+let events = false;
+
+function changePosted() {
+    posted = !posted;
+    postButton.style.pointerEvents = "none";
+    changeView();
+    eventButton.style.pointerEvents = "auto";
+}
+
+function changeEvents() {
+    eventButton.style.pointerEvents = "none";
+    events = !events;
+    postButton.style.pointerEvents = "auto";
+}
+
+function changeView() {
+    removeAll();
+    if(posted) {
+        showPostedPosts();
+    } else if (events) {
+        showEvents();
+    }
+}
 
 async function loadPosts() {
     const response = await fetch(request_path + "/user/load_posted.php", {
@@ -26,6 +55,10 @@ async function loadProfileInfos() {
     return infos;
 }
 
+function openModal() {
+    alert("Modal opened");
+}
+
 async function showPostedPosts() {
     const posts = document.getElementById("posts");
     const photos = await loadPosts();
@@ -35,6 +68,8 @@ async function showPostedPosts() {
     let photo = photos[photo_index];
     //let event = events[event_index];
     let photosDiv = document.getElementById("photos");
+    let template = document.getElementById("template-photos");
+    console.log(template);
     if (photos.length == 0 /*&& events.length == 0*/) {
         posts.innerHTML = "No posts to show";
         console.log("No posts to show");
@@ -42,19 +77,35 @@ async function showPostedPosts() {
         while (photo_index < photos.length /*&& event_index < events.length*/ && photo.event_post == 0) {
             //if (photo.date_posted > event.date_posted) {
             //addNewPost(posts, photo.photo_id, await loadUserImage(photo.poster), photo.poster, photo.photo, photo.description);
-            photosDiv.getElementsByTagName('div').item(photo_index).getElementsByTagName('img').item(0).src = "/static/img/uploads/" + photo.image;
+            let clone = document.importNode(template.content, true);
+            clone.querySelector("#photo-id").src = "/static/img/uploads/" + photo.image;
+            clone.querySelector("#photo-id").onclick = openModal;
+            //clone.getElementsByTagName('div').item(photo_index).getElementsByTagName('img').item(0).src = "/static/img/uploads/" + photo.image;
             /*let img = posts.getElementsByTagName('div').item(2);//.getElementsByTagName('img').item(0);
             console.log(img);
             img.src = "/static/img/uploads/" + photo.photo;*/
             photo_index++;
             photo = photos[photo_index];
             console.log(photo_index);
+            photosDiv.appendChild(clone);
             /*} else {
                 addNewPost(posts, event.event_id, await loadUserImage(event.organizer), event.name, event.image, event.description, event);
                 event_index++;
                 event = events[event_index];
                 console.log(event_index);
             }*/
+        }
+        let i = 0;
+        if(photos.length % 3 == 1) {
+            i = 2;
+        } else if (photos.length % 3 == 2){
+            i = 1;
+        }
+        for (let j = 0; j < i; j++) {
+            let clone = document.importNode(template.content, true);
+            clone.querySelector("#photo-id").src = "/static/img/default-image.png";
+            clone.querySelector("#photo-id").style.visibility = "hidden";   
+            photosDiv.appendChild(clone);
         }
     }
 }
