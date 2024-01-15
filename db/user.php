da4550827831d2d5a11d9b25f4cae7de8f1a4505
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2024-01-15 11:00:16 +0100

    added new field in user

--- db/user.php
+++ db/user.php
@@ -1,16 +1,19 @@
 <?php
 
-namespace User{
+namespace User {
     require("dbtable.php");
     use Firebase\JWT\JWT;
     use Firebase\JWT\Key;
-    class DBUser implements \DBTable {
+
+    class DBUser implements \DBTable
+    {
         private $username;
         private $email;
         private $name;
         private $surname;
         private $birth_date;
-        private $photo;
+        private $profile_photo;
+        private $background;
         private $bio;
         private $phone;
         private $password;
@@ -18,16 +21,30 @@ namespace User{
         private $follows;
         private $followers;
 
-        
+
         // TODO: add relationship to other user
-        public function __construct($username = null, $email = null, $name = null, $surname = null, 
-                $birth_date = null, $photo = null, $bio = null, $phone = null, $password = null, $online = null, $follows = null, $followers = null) {
+        public function __construct(
+            $username = null,
+            $email = null,
+            $name = null,
+            $surname = null,
+            $birth_date = null,
+            $profile_photo = null,
+            $background = null,
+            $bio = null,
+            $phone = null,
+            $password = null,
+            $online = null,
+            $follows = null,
+            $followers = null
+        ) {
             $this->username = $username;
             $this->email = $email;
             $this->name = $name;
             $this->surname = $surname;
             $this->birth_date = $birth_date;
-            $this->photo = $photo;
+            $this->profile_photo = $profile_photo;
+            $this->background = $background;
             $this->bio = $bio;
             $this->phone = $phone;
             $this->password = $password;
@@ -35,14 +52,16 @@ namespace User{
             $this->follows = $follows;
             $this->followers = $followers;
         }
-        public function jsonSerialize() {
+        public function jsonSerialize()
+        {
             return [
                 "username" => $this->username,
                 "email" => $this->email,
                 "name" => $this->name,
                 "surname" => $this->surname,
                 "birth_date" => $this->birth_date,
-                "photo" => $this->photo,
+                "profile_photo" => $this->profile_photo,
+                "background" => $this->background,
                 "bio" => $this->bio,
                 "phone" => $this->phone,
                 "online" => $this->online,
@@ -50,47 +69,66 @@ namespace User{
                 "followers" => $this->followers
             ];
         }
-        public function create_password(string $password) {
+        public function create_password(string $password)
+        {
             $this->password = password_hash($password, PASSWORD_DEFAULT);
         }
         // insert into database
-        public function db_serialize(\DBDriver $driver) {
+        public function db_serialize(\DBDriver $driver)
+        {
             if ($this->password == null) {
                 throw new \Exception("Password not set");
             }
-            $sql = "INSERT INTO user (username, email, name, surname, birth_date, photo, bio, phone, password, online) 
+            $sql = "INSERT INTO user (username, email, name, surname, birth_date, profile_photo, background, bio, phone, password, online) 
                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             try {
-                $driver->query($sql, $this->username, $this->email, $this->name, $this->surname, date($this->birth_date), 
-                        $this->photo, $this->bio, $this->phone, $this->password, $this->online);
+                $driver->query(
+                    $sql,
+                    $this->username,
+                    $this->email,
+                    $this->name,
+                    $this->surname,
+                    date($this->birth_date),
+                    $this->profile_photo,
+                    $this->background,
+                    $this->bio,
+                    $this->phone,
+                    $this->password,
+                    $this->online
+                );
             } catch (\Exception $e) {
                 throw new \Exception("Error while querying the database: " . $e->getMessage());
             }
 
         }
 
-        public function check_password(string $password) {
+        public function check_password(string $password)
+        {
             return password_verify($password, $this->password);
         }
     }
 
-    class DBPartecipation implements \DBTable {
+    class DBPartecipation implements \DBTable
+    {
         private $event_id;
         private $partecipant;
 
-        public function __construct($event_id = null, $partecipant = null) {
+        public function __construct($event_id = null, $partecipant = null)
+        {
             $this->event_id = $event_id;
             $this->partecipant = $partecipant;
         }
 
-        public function jsonSerialize() {
+        public function jsonSerialize()
+        {
             return [
                 "event_id" => $this->event_id,
                 "partecipant" => $this->partecipant
             ];
         }
 
-        public function db_serialize(\DBDriver $driver) {
+        public function db_serialize(\DBDriver $driver)
+        {
             $sql = "INSERT INTO partecipation (event_id, partecipant) VALUES (?, ?)";
             try {
                 $driver->query($sql, $this->event_id, $this->partecipant);
@@ -100,16 +138,19 @@ namespace User{
         }
     }
 
-    class UserUtility {
-        public static function from_form( string $username, string $email, string $name, string $surname, string $birth_date, string $photo, string $bio, string $phone, string $password = "" ){
-            $user = new DBUser($username, $email, $name, $surname, $birth_date, $photo, $bio, $phone);
-            if( $password != "" ){
+    class UserUtility
+    {
+        public static function from_form(string $username, string $email, string $name, string $surname, string $birth_date, string $profile_photo, string $background, string $bio, string $phone, string $password = "")
+        {
+            $user = new DBUser($username, $email, $name, $surname, $birth_date, $profile_photo, $background, $bio, $phone);
+            if ($password != "") {
                 $user->create_password($password);
             }
             return $user;
         }
 
-        public static function from_db_with_username($driver, string $username){
+        public static function from_db_with_username($driver, string $username)
+        {
             $sql = "SELECT u.*, 
             (SELECT COUNT(*) FROM relationship WHERE followed = ?) as follower,
             (SELECT COUNT(*) FROM relationship WHERE follows = ?) as follows
@@ -120,38 +161,64 @@ namespace User{
             } catch (\Exception $e) {
                 throw new \Exception("Error while querying the database: " . $e->getMessage());
             }
-            
+
             if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc();
-                $user = new DBUser($row["username"], $row["email"], $row["name"], $row["surname"], 
-                        $row["birth_date"], $row["photo"], $row["bio"], $row["phone"], $row["password"], $row["online"], $row["follower"], $row["follows"]);
+                $user = new DBUser(
+                    $row["username"],
+                    $row["email"],
+                    $row["name"],
+                    $row["surname"],
+                    $row["birth_date"],
+                    $row["profile_photo"],
+                    $row["background"],
+                    $row["bio"],
+                    $row["phone"],
+                    $row["password"],
+                    $row["online"],
+                    $row["follower"],
+                    $row["follows"]
+                );
             } else {
                 return null;
             }
             return $user;
         }
 
-        public static function from_db_with_username_like( \DBDriver $driver, string $username ): array{
+        public static function from_db_with_username_like(\DBDriver $driver, string $username): array
+        {
             $sql = "SELECT * FROM user WHERE username LIKE ?";
 
             try {
-                $result = $driver->query($sql, "%".$username."%");
+                $result = $driver->query($sql, "%" . $username . "%");
             } catch (\Exception $e) {
                 throw new \Exception("Error while querying the database: " . $e->getMessage());
             }
             $users = array();
             if ($result->num_rows > 0) {
-                for($i = 0; $i < $result->num_rows; $i++){
+                for ($i = 0; $i < $result->num_rows; $i++) {
                     $row = $result->fetch_array();
-                    $user = new DBUser($row["username"], $row["email"], $row["name"], $row["surname"], $row["birth_date"], 
-                            $row["photo"], $row["bio"], $row["phone"], $row["password"], $row["online"]);
+                    $user = new DBUser(
+                        $row["username"],
+                        $row["email"],
+                        $row["name"],
+                        $row["surname"],
+                        $row["birth_date"],
+                        $row["profile_photo"],
+                        $row["background"],
+                        $row["bio"],
+                        $row["phone"],
+                        $row["password"],
+                        $row["online"]
+                    );
                     array_push($users, $user);
                 }
             }
             return $users;
         }
 
-        public static function from_db_with_email(\DBDriver $driver, string $email ){
+        public static function from_db_with_email(\DBDriver $driver, string $email)
+        {
             $user = null;
             $sql = "SELECT * FROM user WHERE email = ?";
             try {
@@ -161,8 +228,19 @@ namespace User{
             }
             if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc();
-                $user = new DBUser($row["username"], $row["email"], $row["name"], $row["surname"], $row["birth_date"], 
-                        $row["photo"], $row["bio"], $row["phone"], $row["password"], $row["online"]);
+                $user = new DBUser(
+                    $row["username"],
+                    $row["email"],
+                    $row["name"],
+                    $row["surname"],
+                    $row["birth_date"],
+                    $row["profile_photo"],
+                    $row["background"],
+                    $row["bio"],
+                    $row["phone"],
+                    $row["password"],
+                    $row["online"]
+                );
             } else {
                 return null;
             }
@@ -170,7 +248,8 @@ namespace User{
 
         }
 
-        public static function from_db_with_phone( \DBDriver $driver, string $phone ){
+        public static function from_db_with_phone(\DBDriver $driver, string $phone)
+        {
             $user = null;
             $sql = "SELECT * FROM user WHERE phone = ?";
             try {
@@ -180,8 +259,19 @@ namespace User{
             }
             if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc();
-                $user = new DBUser($row["username"], $row["email"], $row["name"], $row["surname"], $row["birth_date"], 
-                        $row["photo"], $row["bio"], $row["phone"], $row["password"], $row["online"]);
+                $user = new DBUser(
+                    $row["username"],
+                    $row["email"],
+                    $row["name"],
+                    $row["surname"],
+                    $row["birth_date"],
+                    $row["profile_photo"],
+                    $row["background"],
+                    $row["bio"],
+                    $row["phone"],
+                    $row["password"],
+                    $row["online"]
+                );
             } else {
                 return null;
             }
@@ -189,8 +279,9 @@ namespace User{
 
         }
 
-        public static function check_if_available(\DBDriver $driver, $username="", $email="", $phone=""): void{
-            if(!empty($username)){
+        public static function check_if_available(\DBDriver $driver, $username = "", $email = "", $phone = ""): void
+        {
+            if (!empty($username)) {
                 $sql = "SELECT * FROM user WHERE username = ?";
                 try {
                     $result = $driver->query($sql, $username);
@@ -200,7 +291,7 @@ namespace User{
                 if ($result->num_rows > 0) {
                     throw new UsernameTaken("Username already taken");
                 }
-            } else if(!empty($email)){
+            } else if (!empty($email)) {
                 $sql = "SELECT * FROM user WHERE email = ?";
                 try {
                     $result = $driver->query($sql, $email);
@@ -210,7 +301,7 @@ namespace User{
                 if ($result->num_rows > 0) {
                     throw new EmailTaken("Email already taken");
                 }
-            } else if(!empty($phone)){
+            } else if (!empty($phone)) {
                 $sql = "SELECT * FROM user WHERE phone = ?";
                 try {
                     $result = $driver->query($sql, $phone);
@@ -223,8 +314,9 @@ namespace User{
             }
         }
 
-        public static function retrieve_username_from_token($token): string {
-            if(preg_match("/Bearer\s(\S+)/", $token, $matches) !== 1){
+        public static function retrieve_username_from_token($token): string
+        {
+            if (preg_match("/Bearer\s(\S+)/", $token, $matches) !== 1) {
                 throw new \Exception("Invalid token");
             } else {
                 $token = $matches[1];
@@ -233,7 +325,8 @@ namespace User{
             }
         }
 
-        public static function retrieve_partecipation($driver, $event_id) {
+        public static function retrieve_partecipation($driver, $event_id)
+        {
             $sql = "SELECT * FROM event_partecipation WHERE event_id = ?";
             try {
                 $result = $driver->query($sql, $event_id);
@@ -242,7 +335,7 @@ namespace User{
             }
             $partecipations = array();
             if ($result->num_rows > 0) {
-                for($i = 0; $i < $result->num_rows; $i++){
+                for ($i = 0; $i < $result->num_rows; $i++) {
                     $row = $result->fetch_array();
                     $partecipation = new DBPartecipation($row["event_id"], $row["partecipant"]);
                     array_push($partecipations, $partecipation);
@@ -251,7 +344,8 @@ namespace User{
             return $partecipations;
         }
 
-        public static function retrieve_online_followed($driver, $username) {
+        public static function retrieve_online_followed($driver, $username)
+        {
             $sql = "SELECT u.* FROM user u, relationship r 
                     WHERE u.username = r.followed AND r.follows = ? AND u.online = 1";
             try {
@@ -261,18 +355,30 @@ namespace User{
             }
             $users = array();
             if ($result->num_rows > 0) {
-                for($i = 0; $i < $result->num_rows; $i++){
+                for ($i = 0; $i < $result->num_rows; $i++) {
                     $row = $result->fetch_array();
-                    $user = new DBUser($row["username"], $row["email"], $row["name"], $row["surname"], $row["birth_date"], 
-                            $row["photo"], $row["bio"], $row["phone"], $row["password"], $row["online"]);
+                    $user = new DBUser(
+                        $row["username"],
+                        $row["email"],
+                        $row["name"],
+                        $row["surname"],
+                        $row["birth_date"],
+                        $row["profile_photo"],
+                        $row["background"],
+                        $row["bio"],
+                        $row["phone"],
+                        $row["password"],
+                        $row["online"]
+                    );
                     array_push($users, $user);
                 }
             }
             return $users;
         }
 
-        public static function retrieve_profile_picture($driver, $username) {
-            $sql = "SELECT photo FROM user WHERE username = ?";
+        public static function retrieve_profile_picture($driver, $username)
+        {
+            $sql = "SELECT profile_photo FROM user WHERE username = ?";
             try {
                 $result = $driver->query($sql, $username);
             } catch (\Exception $e) {
@@ -280,15 +386,21 @@ namespace User{
             }
             if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc();
-                return $row["photo"];
+                return $row["profile_photo"];
             } else {
                 return null;
             }
         }
     }
-    class UsernameTaken extends \Exception {}
-    class EmailTaken extends \Exception {}
-    class PhoneTaken extends \Exception {}
+    class UsernameTaken extends \Exception
+    {
+    }
+    class EmailTaken extends \Exception
+    {
+    }
+    class PhoneTaken extends \Exception
+    {
+    }
 }
 
 
