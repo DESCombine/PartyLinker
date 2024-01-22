a4abbb9a59342c070606d6e8fc5108459573670e
Danilo <magliadanilo@gmail.com>
2024-01-22 10:00:29 +0100

    Handled in a better way tags extraction from comment

--- php/requests/email/tag_notify_handler.php
+++ php/requests/email/tag_notify_handler.php
@@ -4,28 +4,35 @@
     require_once(getenv("PL_ROOTDIRECTORY")."db/user.php");
     use User\UserUtility;
 
+
+    /* Hi my name @sevetom @danilo.maglia @manu.sanchi*/ 
     function tag_notify_handler($text) {
         global $driver;
         global $username;
+        $tags = getTags($text);
+        foreach ($tags as $tag) {
 
-        while (strpos($text, '@')) {
-            $tag_start = strpos($text, '@');
-            $tag_end = strpos($text, ' ', $tag_start);
-            if($tag_end === false) {
-                $tag_end = strlen($text);
-            }
-            $tag = substr($text, $tag_start + 1, $tag_end - $tag_start - 1);
             $tagged_user = UserUtility::from_db_with_username($driver, $tag);
             if ($tagged_user) {
-                $notif_text = "Hey @reciever, you have been tagged in a post by @sender!\n
-                        Check it out at partylinker.live";
+                $notif_text = "Hey @reciever, you have been tagged in a post by @sender! Check it out at partylinker.live";
                 $notif_text = str_replace('@reciever', $tagged_user->getUsername(), $notif_text);
                 $notif_text = str_replace('@sender', $username, $notif_text);
                 $to = $tagged_user->getEmail();
                 $subject = "You have been tagged in a post!";
                 $headers = "From: PartyLinker <noreply@partylinker.com>";
-                //mail($to, $subject, $notif_text, $headers);
+                mail($to, $subject, $notif_text, $headers);
             }
         }
+    }
+
+    function getTags($text) {
+        $words = explode(" ", $text);
+        $tags = array();
+        foreach ($words as $word) {
+            if (strpos($word, '@') === 0) {
+                array_push($tags, substr($word, 1));
+            }
+        }
+        return $tags;
     }
 ?>
--- php/test.php
+++ php/test.php
@@ -2,8 +2,7 @@
 require_once(getenv("PL_ROOTDIRECTORY"). "php/bootstrap.php");
 
 require_once(getenv("PL_ROOTDIRECTORY"). "php/img_upload_handler.php");
+require_once(getenv("PL_ROOTDIRECTORY"). "php/requests/email/tag_notify_handler.php");
 
-echo img_handler($_FILES["img"]);
-
-
+tag_notify_handler("Hi my name @sevetom @danilo.maglia @manu.sanchi");
 ?>
