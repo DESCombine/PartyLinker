b01643d427da335de99fc602c1858a5ef8e4a452
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2024-01-25 11:22:14 +0100

    finished mediaquery. TODO: change style inside form

--- static/js/modifyprofile.js
+++ static/js/modifyprofile.js
@@ -1,3 +1,15 @@
 import { request_path } from "/static/js/config.js?v=2";
 
 document.getElementsByTagName("form")[0].action = request_path + "/user/modifyprofile.php";
+
+if (window.matchMedia("(min-width: 767px)").matches) {
+    document.getElementsByTagName('img')[0].classList.add('d-none')
+}
+
+window.addEventListener("resize", function(){
+    if(window.matchMedia("(min-width: 767px)").matches){
+        document.getElementsByTagName('img')[0].classList.add('d-none')
+    } else {
+        document.getElementsByTagName('img')[0].classList.remove('d-none')
+    }
+});
