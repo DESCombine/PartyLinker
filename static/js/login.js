4dc1955a670d6c6fa6d8d12544691af86b75f1b1
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2024-01-25 10:28:28 +0100

    mediaquery finished

--- static/js/login.js
+++ static/js/login.js
@@ -1,26 +1,17 @@
-import { request_path } from "/static/js/config.js?v=2";
+/*if(window.matchMedia("(max-width: 767px)").matches){
+    document.getElementById("extern-container").classList.add("d-flex");
+    document.getElementById("extern-container").classList.add("align-middle");
+}*/
 
-document.getElementsByTagName("form")[0].action = request_path + "/auth/auth.php";
-
-async function tryLogin() {
-    username = document.getElementById("username").value
-    password = document.getElementById("password").value
-    //fetch(request_path + "/auth/auth.php", {
-    const response = await fetch(request_path + "/auth/auth.php", {
-        method: "POST",
-        credentials: "include",
-        header: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "username": username,
-            "password": password
-        })
-    })
-    const data = await response.json()
-    if(!data.hasOwnProperty("error")) {
-        window.location.href = "/"
+// when screen is resized
+window.addEventListener("resize", function(){
+    if(window.matchMedia("(min-width: 767px)").matches){
+        document.getElementById("extern-container").classList.add("d-flex");
+        document.getElementById("extern-container").classList.add("align-middle");
+        this.document.getElementById("first").classList.add("align-items-center");
     } else {
-        //TODO: Show error message
+        document.getElementById("extern-container").classList.remove("d-flex");
+        document.getElementById("extern-container").classList.remove("align-middle");
+        this.document.getElementById("first").classList.remove("align-items-center");
     }
-}
+});
