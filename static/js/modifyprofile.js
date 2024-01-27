a1d07408c469955ba218b42641f2953ba26b6ed6
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2024-01-27 10:50:56 +0100

    fixed mediaquery to have content vertically centered on large screen

--- static/js/modifyprofile.js
+++ static/js/modifyprofile.js
@@ -9,7 +9,9 @@ if (window.matchMedia("(min-width: 767px)").matches) {
 window.addEventListener("resize", function(){
     if(window.matchMedia("(min-width: 767px)").matches){
         document.getElementsByTagName('img')[0].classList.add('d-none')
+        this.document.getElementById('extern-container').classList.add('d-flex')
     } else {
         document.getElementsByTagName('img')[0].classList.remove('d-none')
+        this.document.getElementById('extern-container').classList.remove('d-flex')
     }
 });
