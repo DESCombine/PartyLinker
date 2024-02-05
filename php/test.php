b17b6ca3cd97b8bb4817a0c6e3818df84cab31e6
sevetom <sevetom2611@libero.it>
2024-02-05 12:50:12 +0100

    added test file

--- php/test.php
+++ php/test.php
@@ -1,5 +1,10 @@
 <?php 
     require_once(getenv("PL_ROOTDIRECTORY"). "php/bootstrap.php");
-    require_once(getenv("PL_ROOTDIRECTORY"). "php/tfa_handler.php");
-    tfa_send("danilo.maglia");
+    require_once(getenv("PL_ROOTDIRECTORY"). "php/requests/email/comment_notify_handler.php");
+    require_once(getenv("PL_ROOTDIRECTORY"). "php/requests/email/follow_notify_handler.php");
+    require_once(getenv("PL_ROOTDIRECTORY")."php/requests/email/like_notify_handler.php");
+    follow_notify_handler("sevetom");
+    comment_notify_handler(14);
+    like_notify_handler(14, "post");
+    like_notify_handler(55, "comment");
 ?>
